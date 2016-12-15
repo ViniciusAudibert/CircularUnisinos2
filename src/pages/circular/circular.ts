7fa2f62e581cf4fbbfd54f73151efe3bf98551c2
Vinicius Audibert <vinicius.audibert@gmail.com>
2016-12-15 14:35:45 -0200

    feat(circular): add function to generate array from object

--- src/pages/circular/circular.ts
+++ src/pages/circular/circular.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { CircularService } from './circular.service';
 import { NavController } from 'ionic-angular';
 
@@ -11,17 +12,25 @@ export class Circular implements OnInit {
   public chosenWeek: any;
   public time: any;
   public circularInfo: any;
-  public circularServiceInfo;
+  public circularHours: any;
 
   constructor(public navCtrl: NavController, private circularService: CircularService) {}
 
   getCircularInfo(): void {
-    this.circularInfo = this.circularService.getCircularInfoByHour(this.circularServiceInfo,2, null, null);
+    this.circularInfo = this.circularService.getCircularInfoByHour(this.circularHours,2, null, null);
   }
 
   ngOnInit(): void {
-  this.circularServiceInfo = this.circularService.getCircularInfo();
+  this.circularHours = this.circularService.getCircularInfo()['__zone_symbol__value'];
   this.getCircularInfo();
 }
+
+  generateArray(obj){
+    return Object.keys(obj).map((key)=>{ return obj[key]});
+  }
+
+  getKeyByIndex(obj, index){
+    return Object.keys(obj)[index];
+  }
 
 }
